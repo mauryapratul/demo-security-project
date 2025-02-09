9d974d81ebd2600c3c3eee3682a6b605b9f88d4f
Pratul <pratulmaurya@gmail.com>
2025-02-09 20:19:02 +0530

    changes made for custom username and password for login in application.properties.

--- src/main/java/com/sec/demo/controller/DemoController.java
+++ src/main/java/com/sec/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.sec.demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
