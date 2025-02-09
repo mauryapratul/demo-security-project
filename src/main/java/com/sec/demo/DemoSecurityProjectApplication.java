04f8f615032a3e2faf959a94dbdd265e5be1f095
Pratul <pratulmaurya@gmail.com>
2025-02-09 19:53:28 +0530

    for based security

---
+++ src/main/java/com/sec/demo/DemoSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sec/demo/DemoSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
