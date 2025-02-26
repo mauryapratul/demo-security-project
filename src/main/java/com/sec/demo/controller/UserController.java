933bca91023faf9c8761938c317a6cd928401645
Pratul <pratulmaurya@gmail.com>
2025-02-26 12:09:28 +0530

    making few urls like login out of security scope so that anybody can access that

--- src/main/java/com/sec/demo/controller/UserController.java
+++ src/main/java/com/sec/demo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.sec.demo.controller;
 import com.sec.demo.model.User;
 import com.sec.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService service;
+    @Autowired
+    AuthenticationManager authenticationManager;
    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
     @PostMapping("register")
     public User register(@RequestBody User user){
         user.setPassword(encoder.encode(user.getPassword()));
         return service.saveUser(user);
 
+    }
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication = authenticationManager.
+                authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+        if(authentication.isAuthenticated()){
+            return "Success";
+        }
+        else
+            return "Login Failed";
     }
 }
